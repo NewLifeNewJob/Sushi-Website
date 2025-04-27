dec800a1c326ea83ea76b2b02915e4bb02456026
NightKnight32 <92237560+NightKnight32@users.noreply.github.com>
2025-04-26 21:11:42 -0400

    collapsible added

---
+++ js/main.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const toggleButton = document.querySelector('[data-toggle="collapsible"]');
+  const collapsibleContent = document.querySelector(".collapsible_content");
+
+  toggleButton.addEventListener("click", () => {
+    collapsibleContent.classList.toggle("active");
+  });
+});
+
+const collapsibles = document.querySelectorAll(".collapsible");
+collapsibles.forEach((item) =>
+  item.addEventListener("click", function () {
+    this.classList.toggle("collapsible--expanded");
+  })
+);
